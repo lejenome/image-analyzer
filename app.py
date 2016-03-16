1bdb57f7340ab4580f3ee6cc70e2d2ab2d76d962
Moez Bouhlel <bmoez.j@gmail.com>
2016-03-16 18:45:18 +0100

    imporve FileChooserDialog Filters

--- app.py
+++ app.py
@@ -6,34 +6,34 @@ from gi.repository import Gtk, Gdk, GdkPixbuf
 builder = Gtk.Builder()
 builder.add_from_file("app.glade")
 
-image_filter = builder.get_object('filefilter1')
-image_filter.set_name("Image files")
-image_filter.add_pixbuf_formats()
-any_filter = builder.get_object('filefilter2')
-any_filter.set_name("Any files")
-any_filter.add_mime_type("*")
-
 win = builder.get_object('window1')
 win.about = builder.get_object('aboutdialog1')
 
 imageList = builder.get_object("listbox1")
 imageView = builder.get_object('image1')
 
+# imageList.set_sort_func(lambda r1, r2, data, notify_destroy: return -1, None, False)
+# imageList.set_filter_func(lambda r, r2, data, notify_destroy: return True, None, False)
+
 # ajouter image dans View
-def showImage(name):
+def show_image(name):
     size = imageView.get_allocation()
     pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(name, size.width, size.height)
     imageView.set_from_pixbuf(pixbuf)
 # ajouter image dans la liste
-def addImage(name):
+def add_image(name):
     it = Gtk.ListBoxRow()
     it.data = name
     it.add(Gtk.Label(name.split('/')[-1]))
     imageList.add(it)
+def add_images(names):
+    for name in names:
+        add_image(name)
     imageList.show_all()
-    showImage(name)
+    show_image(names[-1])
 
-imageList.connect('row-activated', lambda w, row: showImage(row.data))
+
+imageList.connect('row-activated', lambda w, row: show_image(row.data))
 
 class EventHandler():
 
@@ -48,15 +48,30 @@ class EventHandler():
                                         Gtk.FileChooserAction.OPEN,
                                         (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
                                          Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+        chooser.set_select_multiple(True)
+
+        image_filter = Gtk.FileFilter()
+        image_filter.set_name("Image files")
+        image_filter.add_pixbuf_formats()
+        any_filter = Gtk.FileFilter()
+        any_filter.set_name("Any files")
+        any_filter.add_pattern("*")
+
         chooser.add_filter(image_filter)
         chooser.add_filter(any_filter)
         response = chooser.run()
         if response == Gtk.ResponseType.OK:
-            addImage(chooser.get_filename())
+            add_images(chooser.get_filenames())
         chooser.destroy()
 
     def on_about_clicked(self, *args):
         win.about.show_all()
+        # .run
+        # .destroy
+
+    def on_about_closed(self, *args):
+        print("on_about_closed")
+        win.about.hide()
 
 builder.connect_signals(EventHandler())
 win.show_all()
